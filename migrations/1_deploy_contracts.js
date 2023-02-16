4c12eb54d8c5d7cf743e1a15cf07b18e0128f720
napascual <napascual@gmail.com>
2023-02-16 18:21:14 +0100

    feat: remove ConvertLib deployment

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require('ConvertLib');
 const Testament = artifacts.require('Testament');
 
 module.exports = function(deployer) {
-    deployer.deploy(ConvertLib);
-    deployer.link(ConvertLib, Testament);
     deployer.deploy(Testament);
 };
